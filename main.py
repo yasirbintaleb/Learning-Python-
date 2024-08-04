6b45ca46ec1988c2c2b5f00b0cde6ff8e33108ee
Mohammad Yasir Bin Taleb Abrar <157371109+yasirbintaleb@users.noreply.github.com>
2024-08-04 12:40:46 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Python programming Day 1
+print("Hello World", "this is yasir")
+print("This is my first program")
+print("I am from Chattogram, Bangladesh")
+
+# Use of Escape Sequence Character
+print("I study at \"Chandigarh University\" which is in \nMohali, India")
+
+# Use of More print statements
+print("My date of birth is :", 22, 11, 2003, sep="-")
+
+# Use the Data Type 
+a=1300056341
+b="This is my mobile number"
+print(b,a, sep=":")
+
+# Mapped Data: dict
+dict1={"Fathers Name": "Mohammad Abu Taleb", "Mothers Name": "Shaon Akther"}
+print(dict1)
